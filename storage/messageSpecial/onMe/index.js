45fef8f3e7bcae10e1ffd0a18b70e10e0acc961a
CodyIsTheSenate <mathieuman78@gmail.com>
2021-03-14 13:28:06 +0100

    onMe is now working when the bot isn't already in a voice channel

--- storage/messageSpecial/onMe/index.js
+++ storage/messageSpecial/onMe/index.js
@@ -1,24 +1,32 @@
+function start(bot, guildId, channelId) {
+    bot.musicFunctions.get("startBotMusicInGuilds").one(bot, guildId, (error, results, fields) => {
+        bot.musicFunctions.get("createPlaylist").run(bot, guildId, () => {
+            bot.musicFunctions.get("joinVoiceChannel").run(bot, channelId);
+        });
+    });
+}
+
 module.exports.addReaction = async (bot, reaction, user, messageData, index) => {
     reaction.users.remove(user.id);
 
     const member = await reaction.message.guild.members.fetch(user.id);
     const channel = member.voice.channel;
     if (channel) {
-        reaction.message.guild.me.voice.channel.join().then(async (connection) => {
-            if (connection.dispatcher) connection.dispatcher.destroy();
-            connection.disconnect();
+        const actualChannel = reaction.message.guild.me.voice.channel;
+        if (actualChannel) {
+            reaction.message.guild.me.voice.channel.join().then(async (connection) => {
+                if (connection.dispatcher) connection.dispatcher.destroy();
+                connection.disconnect();
 
-
-            bot.musicFunctions.get("startBotMusicInGuilds").one(bot, reaction.message.guild.id, (error, results, fields) => {
-                bot.musicFunctions.get("createPlaylist").run(bot, reaction.message.guild.id, () => {
-                    bot.musicFunctions.get("joinVoiceChannel").run(bot, channel.id);
-                });
+                start(bot, reaction.message.guild.id, channel.id);
             });
-        });
-    }else{
+        } else {
+            start(bot, reaction.message.guild.id, channel.id);
+        }
+    } else {
         reaction.message.channel.send("<@" + user.id + "> tu n'es pas dans un salon vocal.").then(async (msg) => {
             await bot.basicFunctions.get("wait").run(5000);
-            if(msg.deletable) msg.delete();
+            if (msg.deletable) msg.delete();
         })
     }
 }
