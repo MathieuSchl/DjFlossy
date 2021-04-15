1093d38b6b27ab33ddc7d41e2a57de10659a0fe7
CodyIsTheSenate <mathieuman78@gmail.com>
2021-04-15 23:44:16 +0200

    Test to catch error no video find with ytdl library

--- storage/musicFunctions/startPlayingMusic.js
+++ storage/musicFunctions/startPlayingMusic.js
@@ -11,53 +11,60 @@ async function getSong(bot, idSong, callback) {
 module.exports.run = async (bot, voiceChannel, connection) => {
     bot.musicFunctions.get("getNextSongId").run(bot, voiceChannel.guild.id, async (idSong) => {
         getSong(bot, idSong, async (error, results, fields) => {
-            if (error) throw error;
+            try {
+                if (error) throw error;
 
-            const songData = results[0];
-            if (songData == null) {
-                console.log(`The song with id "${idSong}" does not exist`);
-                return;
-            }
+                const songData = results[0];
+                if (songData == null) {
+                    console.log(`The song with id "${idSong}" does not exist`);
+                    return;
+                }
 
 
-            const streamOptions = {
-                volume: songData.volume
-            };
-            const videoData = await ytdl.getInfo(songData.tagName);
-            const stream = ytdl(songData.tagName, {
-                quality: 'lowestaudio',
-                filter: 'audioonly'
-            });
+                const streamOptions = {
+                    volume: songData.volume
+                };
+                //const videoData = await ytdl.getInfo(songData.tagName);
+                const stream = ytdl(songData.tagName, {
+                    quality: 'lowestaudio',
+                    filter: 'audioonly'
+                })
 
-            const dispatcher = connection.play(stream, streamOptions);
+                const dispatcher = connection.play(stream, streamOptions);
 
-            dispatcher.on("finish", () => {
-                dispatcher.destroy();
+                dispatcher.on("finish", () => {
+                    dispatcher.destroy();
+                    bot.musicFunctions.get("startPlayingMusic").run(bot, voiceChannel, connection);
+                    return;
+                    //voiceChannel.leave();
+                });
+
+                connection.on("disconnect", () => {
+                    dispatcher.destroy();
+                    voiceChannel.leave();
+                })
+
+                dispatcher.on("error", (err) => {
+                    console.log("Error with the dispatcher");
+                    console.log(err.code);
+                    console.log("\n\n");
+                    console.log(err);
+                    dispatcher.destroy();
+                    voiceChannel.leave();
+                });
+
+                /*
+                await bot.basicFunctions.get("wait").run(videoData.videoDetails.lengthSeconds * 1100);
+                if (!dispatcher["_writableState"].ended) {
+                    dispatcher.destroy();
+                }
+                */
+
+            } catch {
+                console.log("Error in startPlayingMusic");
+                console.log(e);
                 bot.musicFunctions.get("startPlayingMusic").run(bot, voiceChannel, connection);
-                return;
-                //voiceChannel.leave();
-            });
-
-            connection.on("disconnect", () => {
-                dispatcher.destroy();
-                voiceChannel.leave();
-            })
-
-            dispatcher.on("error", (err) => {
-                console.log("Error with the dispatcher");
-                console.log(err.code);
-                console.log("\n\n");
-                console.log(err);
-                dispatcher.destroy();
-                voiceChannel.leave();
-            });
-
-            /*
-            await bot.basicFunctions.get("wait").run(videoData.videoDetails.lengthSeconds * 1100);
-            if (!dispatcher["_writableState"].ended) {
-                dispatcher.destroy();
             }
-            */
         });
     });
 };
