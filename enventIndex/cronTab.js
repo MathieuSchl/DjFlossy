1de969a9a6db73b5c85d634e9a282ce44f2be0b5
CodyIsTheSenate <mathieuman78@gmail.com>
2021-05-24 23:43:33 +0200

    Add a launcher for cronTable from DB

--- enventIndex/cronTab.js
+++ enventIndex/cronTab.js
@@ -1,5 +1,53 @@
 const Discord = require("discord.js");
+var CronJob = require('cron').CronJob;
 
+
+async function startCron(bot, data) {
+    const user = await bot.users.fetch(data.user);
+
+    const job = new CronJob(data.cronSchedule, async function () {
+        user.send(data.data.mess);
+
+        if (!data.repetitive) {
+            job.stop();
+            const dbPrefix = await bot.basicFunctions.get("DbConfiguration").getDbPrefix(bot);
+            bot.dataBase.get("connection").exec(bot.db, 'DELETE FROM ?? WHERE `id` = ?', [dbPrefix + "cronTables", data.id], async (error, results, fields) => {
+                for (let index = 0; index < results.length; index++) {
+                    results[index].data = JSON.parse(results[index].data);
+
+                    const theCron = results[index];
+
+                    const res = await startCron(bot, theCron);
+                    bot.cronTab.set(res.name, res.job);
+                    bot.cronTab.get(res.name).start();
+                }
+                return;
+            });
+        }
+    });
+
+    return {
+        "name": data.type + ">" + user.username,
+        "job": job
+    };
+}
+
+
+async function startMPCron(bot) {
+    const dbPrefix = await bot.basicFunctions.get("DbConfiguration").getDbPrefix(bot);
+    bot.dataBase.get("connection").exec(bot.db, 'SELECT * FROM ?? WHERE `type` = "MP"', [dbPrefix + "cronTables"], async (error, results, fields) => {
+        for (let index = 0; index < results.length; index++) {
+            results[index].data = JSON.parse(results[index].data);
+
+            const theCron = results[index];
+
+            const res = await startCron(bot, theCron);
+            bot.cronTab.set(res.name, res.job);
+            bot.cronTab.get(res.name).start();
+        }
+        return;
+    });
+}
 
 module.exports.run = async (bot) => {
     let listCron = Array.from(bot.cronTable);
@@ -10,6 +58,8 @@ module.exports.run = async (bot) => {
         bot.cronTab.set(res.name, res.job);
         bot.cronTab.get(res.name).start();
     }
+
+    startMPCron(bot);
 };
 
 module.exports.stop = async (bot) => {
