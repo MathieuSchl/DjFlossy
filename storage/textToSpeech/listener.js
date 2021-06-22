e5ef8768a90e85a18bbdb1f794e203657f749bde
CodyIsTheSenate <mathieuman78@gmail.com>
2021-06-23 00:24:04 +0200

    Fix error with write system for listener

--- storage/textToSpeech/listener.js
+++ storage/textToSpeech/listener.js
@@ -1,9 +1,7 @@
 const fs = require('fs');
 const config = require('../config.json');
-const witClient = require('node-witai-speech');
-const minDuration = 0.7;
-const maxDuration = 5.0;
-const FileWriter = require('wav').FileWriter;
+var wavConverter = require('wav-converter')
+var path = require('path')
 
 
 function makeid(length) {
@@ -16,58 +14,6 @@ function makeid(length) {
     return result.join('');
 }
 
-async function convert_audio(input) {
-    try {
-        // stereo to mono channel
-        const data = new Int16Array(input)
-        const ndata = new Int16Array(data.length / 2)
-        for (let i = 0, j = 0; i < data.length; i += 4) {
-            ndata[j++] = data[i]
-            ndata[j++] = data[i + 1]
-        }
-        return Buffer.from(ndata);
-    } catch (e) {
-        console.log(e)
-        console.log('convert_audio: ' + e)
-        throw e;
-    }
-}
-
-
-async function test(fileName, callBack) {
-    try {
-        var stream = fs.createReadStream(fileName);
-
-        // The content-type for this audio stream (audio/wav, ...)
-        var content_type = "audio/wav";
-
-        // Its best to return a promise
-        var parseSpeech = new Promise((ressolve, reject) => {
-            // call the wit.ai api with the created stream
-            witClient.extractSpeechIntent(config.witaiToken, stream, content_type,
-                (err, res) => {
-                    if (err) return reject(err);
-                    ressolve(res);
-                });
-        });
-
-        // check in the promise for the completion of call to witai
-        parseSpeech.then((data) => {
-                fs.unlinkSync(fileName);
-                if (data.text) callBack(data.text);
-            })
-            .catch((err) => {
-                fs.unlinkSync(fileName);
-                //console.log(err);
-            });
-    } catch (e) {
-        fs.unlinkSync(fileName);
-    }
-}
-
-
-
-
 module.exports.run = async (bot, connection, callBack) => {
     connection.on('speaking', async (user, speaking) => {
         if (speaking.bitfield == 0 || user.bot) {
@@ -75,46 +21,34 @@ module.exports.run = async (bot, connection, callBack) => {
         }
         // this creates a 16-bit signed PCM, stereo 48KHz stream
         const audioStream = connection.receiver.createStream(user, {
-            mode: 'pcm'
+            mode: 'pcm',
+            end: "silence"
         })
-        audioStream.on('error', (e) => {
-            console.log('audioStream: ' + e)
-        });
-        let buffer = [];
-        audioStream.on('data', (data) => {
-            buffer.push(data)
+
+        const theId = makeid(20)
+        const file = config.location + "/storage/data/sounds/output_" + theId;
+        const writer = audioStream.pipe(fs.createWriteStream(file + ".pcm"))
+        writer.on("finish", () => {
+            var pcmData = fs.readFileSync(path.resolve(file + ".pcm"))
+            var wavData = wavConverter.encodeWav(pcmData, {
+                numChannels: 1,
+                sampleRate: 95000,
+                byteRate: 16
+            })
+
+            fs.writeFile(path.resolve(file + ".wav"), wavData, async () => {
+                fs.unlinkSync(file + ".pcm")
+
+                try {
+                    bot.textToSpeech.get("speechToText").run(file + ".wav", async (voiceMessage) => {
+                        callBack(voiceMessage, user);
+                    });
+                } catch (e) {
+                    console.log('tmpraw rename: ' + e)
+                }
+            })
         })
-        audioStream.on('end', async () => {
-            buffer = Buffer.concat(buffer);
-            const duration = buffer.length / 48000 / 4;
-
-            if (duration < minDuration || duration > maxDuration) {
-                return;
-            }
-
-            const soundId = makeid(20);
-            const fileName = config.location + "/storage/data/sounds/output_" + soundId + ".wav";
-            var outputFileStream = new FileWriter(fileName, {
-                sampleRate: 22000,
-                channels: 1
-            });
-
-
-            let new_buffer = await convert_audio(await convert_audio(buffer));
-
-            outputFileStream.write(new_buffer);
-            await bot.basicFunctions.get("wait").run(duration * 25);
-
-            outputFileStream.end();
-
-            try {
-                bot.textToSpeech.get("speechToText").run(fileName, async (voiceMessage) => {
-                    callBack(voiceMessage, user);
-                });
-            } catch (e) {
-                console.log('tmpraw rename: ' + e)
-            }
-        });
+        return;
     });
 };
 
