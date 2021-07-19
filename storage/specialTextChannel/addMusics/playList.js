aa1fab9337bf926f29c3eee85502a234a8c3429a
CodyIsTheSenate <mathieuman78@gmail.com>
2021-07-19 11:39:09 +0200

    New song message is send when a user add a music

--- storage/specialTextChannel/addMusics/playList.js
+++ storage/specialTextChannel/addMusics/playList.js
@@ -1,7 +1,43 @@
 const Discord = require('discord.js');
+const ytdl = require("ytdl-core");
+const ytch = require('yt-channel-info')
+const config = require('../../config.json');
 
 
-function addSong(bot, songId, args) {
+async function testChannelInfo(authorId) {
+    try {
+        await ytch.getChannelInfo(authorId);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+async function sendMusicsInfo(bot, channel, tagName, playlists, user) {
+    const ytInfo = await ytdl.getInfo(tagName);
+
+    const URL = ytInfo.videoDetails.video_url;
+    const avatarURL = await testChannelInfo(ytInfo.videoDetails.author.id) ? (await ytch.getChannelInfo(ytInfo.videoDetails.author.id)).authorThumbnails[2].url : null;
+
+    const musicEmbed = new Discord.MessageEmbed()
+        .setColor('#FF0000')
+        .setTitle(ytInfo.videoDetails.title)
+        .setURL(URL)
+        .setAuthor(ytInfo.videoDetails.author.name, avatarURL, ytInfo.videoDetails.author.channel_url)
+        .setImage(ytInfo.videoDetails.thumbnails[2].url)
+        .setTimestamp()
+        .setFooter(user.tag, await user.avatarURL());
+
+    musicEmbed.setDescription("Musique ajouté dans les playlists:\n");
+    for (let index = 0; index < playlists.length; index++) {
+        const element = playlists[index];
+        musicEmbed.setDescription(musicEmbed.description + "-`" + element + "`\n");
+    }
+
+    channel.send(musicEmbed)
+}
+
+function addSong(bot, songId, args, author) {
     bot.dataBase.get("connection").exec(bot.db, "SELECT `id` FROM ?? WHERE `tagName` = ?", ["musicsList", songId], async (error, results, fields) => {
         if (error) throw error;
         if (results.length !== 0) return;
@@ -13,6 +49,8 @@ function addSong(bot, songId, args) {
                 if (error) throw error;
 
                 const id = results[0].id;
+                const channel = await bot.channels.fetch(config.idNewSongsChannel);
+                sendMusicsInfo(bot, channel, songId, args, author)
                 for (let index = 0; index < args.length; index++) {
                     bot.dataBase.get("connection").exec(bot.db, "SELECT id FROM ?? WHERE `name` = ?", ["musicTag", args[index]], async (error, results, fields) => {
                         if (error) throw error;
@@ -71,12 +109,12 @@ module.exports.run = async (bot, message, dataSpecialChannel) => {
             await bot.basicFunctions.get("wait").run(250);
 
             if (dataSpecialChannel.data.musicTag.length < 20) {
-                addSong(bot, dataSpecialChannel.data.musicTag, args);
+                addSong(bot, dataSpecialChannel.data.musicTag, args, message.author);
             } else {
                 bot.basicFunctions.get("getVideosFromYtPlaylist").run(dataSpecialChannel.data.musicTag, async (playListTags) => {
                     for (let index = 0; index < playListTags.length; index++) {
                         const element = playListTags[index];
-                        addSong(bot, element, args);
+                        addSong(bot, element, args, message.author);
                         await bot.basicFunctions.get("wait").run(100);
                     }
                 });
